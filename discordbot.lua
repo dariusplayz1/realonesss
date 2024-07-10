e2dbe7b07a6c987f0e5702081e2ed6c4c15d0f96
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-07-10 12:00:12 +0300

    Update and rename discordbott.lua to discordbot.lua

--- discordbot.lua
+++ discordbot.lua
@@ -3841,8 +3841,7 @@ do
         --
         local slider = {min = min, max = max, Disabled = false, sub = sub, decimals = decimals, axis = section.currentAxis, current = -99999, holding = false}
         --
-        if name then
-       
+  
 
         if name then
              
