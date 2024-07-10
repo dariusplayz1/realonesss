63205c68c946453fd4d0c3542dc852253d20a96b
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-07-10 19:05:40 +0300

    Update and rename discordbotttt.lua to discordbot.lua

--- discordbot.lua
+++ discordbot.lua
@@ -2093,8 +2093,12 @@ do
             --
             local toggle = self
   
-            local ColorButton = Instance.new("ImageButton")
- 
+            local valsat = Instance.new("ImageLabel")
+            local dot = Instance.new("Frame")
+            local colorhue = Instance.new("ImageLabel")
+            local color = Instance.new("Frame")
+            local transval = Instance.new("ImageLabel")
+            local trans = Instance.new("Frame")
             
             ColorButton.Name = "ColorButton"
             ColorButton.Parent = toggle.addons
@@ -2235,22 +2239,17 @@ do
                         colorpicker.open = not colorpicker.open
                         
 
-                        local UICorner = Instance.new("UICorner")
+		        local UICorner = Instance.new("UICorner")
                         local UIGradient = Instance.new("UIGradient")
                         local TextLabel = Instance.new("TextLabel")
-                        local valsat = Instance.new("ImageLabel")
-                        local dot = Instance.new("Frame")
                         local Trigger = Instance.new("TextButton")
-                        local colorhue = Instance.new("ImageLabel")
-                        local color = Instance.new("Frame")
                         local Trigger_2 = Instance.new("TextButton")
-                        local transval = Instance.new("ImageLabel")
-                        local trans = Instance.new("Frame")
                         local ImageLabel = Instance.new("ImageLabel")
                         local Trigger_3 = Instance.new("TextButton")
                         local ColorPicker = Instance.new("Frame")
                         local UIGradient_2 = Instance.new("UIGradient")
                         local UIGradient_3 = Instance.new("UIGradient")
+
 
                         --Properties:
 
